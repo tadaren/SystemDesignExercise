ce30050ef97747291654e9879604753275858f43
tadaren <ptymaato@yahoo.co.jp>
2020-11-25 09:09:40 +0900

    WindowControllerの変数を変わるようにした

--- parent/window_controller.py
+++ parent/window_controller.py
@@ -7,7 +7,9 @@ class WindowController:
         self.is_open = False
 
     def open(self):
+        self.is_open = True
         print('open window')
 
     def close(self):
+        self.is_open = False
         print('close window')
