81c4b109283e0ce643bdcd22bf9316b7e576ebf2
tadaren <ptymaato@yahoo.co.jp>
2020-12-09 11:13:57 +0900

    buildした時にapiのURLパスが書き換わるのを直した

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -52,7 +52,9 @@ export default {
   },
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
+  axios: {
+    proxy: true
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
