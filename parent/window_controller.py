0836481b7cbd51a327a04f700abd84f8b16d5b27
tadaren <ptymaato@yahoo.co.jp>
2020-12-20 22:38:22 +0900

    子機にメッセージを送るところを実装

--- parent/window_controller.py
+++ parent/window_controller.py
@@ -5,11 +5,14 @@ class WindowController:
     def __init__(self):
         self.open_limit = 100
         self.is_open = False
+        self.CHILD_IP = '192.168.11.2:8080'
 
     def open(self):
         self.is_open = True
         print('open window')
+        requests.post(f'http://{self.CHILD_IP}/open', str(self.open_limit))
 
     def close(self):
         self.is_open = False
         print('close window')
+        requests.post(f'http://{self.CHILD_IP}/close')
