ef336c1241cd718b1300cfdc20be1d827301ac91
tadaren <ptymaato@yahoo.co.jp>
2020-12-22 15:08:01 +0900

    テストを追加

---
+++ parent/test/test_notice_sender.py
@@ -0,0 +1,8 @@
+import unittest
+from notice_sender import NoticeSender
+
+class TestNoticeSender(unittest.TestCase):
+    def test_send(self):
+        notice_sender = NoticeSender()
+        res = notice_sender.send('テスト用メッセージ')
+        self.assertEqual(res, None)
---
+++ parent/test/test_ventilation.py
@@ -0,0 +1,58 @@
+import unittest
+import unittest.mock
+from ventilation import Ventilation
+from notice_sender import NoticeSender
+
+class TestVentilationRun(unittest.TestCase):
+    def setUp(self):
+        self.sender = unittest.mock.MagicMock()
+        self.window_controller = unittest.mock.MagicMock()
+
+    
+    @unittest.mock.patch('time.sleep', return_value=None)
+    def test_down(self, sleep_mock):
+        sensor_mock = unittest.mock.MagicMock()
+        sensor_mock.get_humid.side_effect = [50.0, 33.0]
+        ventilation = Ventilation(self.window_controller, self.sender, sensor_mock, {})
+        ventilation.run()
+        self.window_controller.open.assert_called_once()
+        self.window_controller.close.assert_called_once()
+        self.sender.assert_not_called()
+        sleep_mock.assert_called_once()
+
+    @unittest.mock.patch('time.sleep', return_value=None)
+    def test_up_over_30(self, sleep_mock):
+        sensor_mock = unittest.mock.MagicMock()
+        sensor_mock.get_humid.side_effect = [10.0, 33.0]
+        ventilation = Ventilation(self.window_controller, self.sender, sensor_mock, {})
+        ventilation.run()
+        self.window_controller.open.assert_called_once()
+        self.window_controller.close.assert_called_once()
+        sleep_mock.assert_called_once()
+        self.sender.assert_not_called()
+
+    @unittest.mock.patch('time.sleep', return_value=None)
+    def test_up_reach_30(self, sleep_mock):
+        sensor_mock = unittest.mock.MagicMock()
+        sensor_mock.get_humid.side_effect = [10.0, 15.0, 17.0, 20.0, 28.0, 33.0]
+        ventilation = Ventilation(self.window_controller, self.sender, sensor_mock, {})
+        ventilation.run()
+        self.window_controller.open.assert_called_once()
+        self.window_controller.close.assert_called_once()
+        self.assertEqual(sleep_mock.call_count, 5)
+        self.sender.assert_not_called()
+
+
+    @unittest.mock.patch('time.sleep', return_value=None)
+    @unittest.mock.patch('time.time', side_effect=[0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480, 510, 540, 570, 600, 630, 660])
+    def test_up_under_30(self, time_mock, sleep_mock):
+        sensor_mock = unittest.mock.MagicMock()
+        sensor_mock.get_humid.side_effect = [10.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0, 11.0]
+        ventilation = Ventilation(self.window_controller, self.sender, sensor_mock, {})
+        ventilation.run()
+        self.window_controller.open.assert_called_once()
+        self.window_controller.close.assert_called_once()
+        self.sender.send.assert_called_once_with('湿度が低下しています')
+        self.assertEqual(sleep_mock.call_count, 20)
+
+
