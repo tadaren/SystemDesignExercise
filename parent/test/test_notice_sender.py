9eac6e084760bcf674d4eed91f8d2b1a8953775f
tadaren <ptymaato@yahoo.co.jp>
2020-12-22 15:16:03 +0900

    テスト用のメッセージを変更

--- parent/test/test_notice_sender.py
+++ parent/test/test_notice_sender.py
@@ -4,5 +4,5 @@ from notice_sender import NoticeSender
 class TestNoticeSender(unittest.TestCase):
     def test_send(self):
         notice_sender = NoticeSender()
-        res = notice_sender.send('テスト用メッセージ')
+        res = notice_sender.send('湿度が30%以下です')
         self.assertEqual(res, None)
